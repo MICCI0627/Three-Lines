6befa0616cc5d996be1a9e863bd6a516ec176ab6
Yoshihiro Mitsuyuki <mitsuyuki@YoshihironoMacBook-puro.local>
2019-02-25 00:19:28 +0900

    Add migration,table and model files

---
+++ database/migrations/2019_02_24_144802_create_shares_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSharesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shares', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('url', 20);
+            $table->string('text1', 30);
+            $table->string('text2', 30);
+            $table->string('text3', 30);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shares');
+    }
+}
